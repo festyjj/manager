89feb3264bf0a4c24c1b2611713f319860a89a03
esty f <3338431675@qq.com>
2026-01-14 20:24:01 +0800

    Visualization

--- src/api/carbonReport/saleStat.js
+++
@@ -1,9 +0,0 @@
-
-import request from '@/utils/request'
-export function saleStat(query) {
-    return request({
-        url: '/carbonReport/saleStat',
-        method: 'get',
-        params: query
-    })
-}
